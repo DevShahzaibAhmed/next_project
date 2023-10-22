8b6946ef147e9e4fb6cd7b28656edbaf4a5fbdc9
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-23 01:04:13 +0500

    updated

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
         </div>
         <Searchform/>
       </section>
-
+    
     </main>
   )
 }
--- components/ui/Searchform.tsx
+++ components/ui/Searchform.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import { Input } from "@/components/ui/input"
+
 import { form } from 'sanity/desk'
 
 const Searchform = () => {
@@ -8,8 +10,12 @@ const Searchform = () => {
         <label>
             <Image src="/magnifying-glass.svg" className="absolute left-8"height={32} width={32} 
             alt="Search-icon"/>
+        
+        <Input />
+
         </label>
     </form>
+
 
   )
 }
