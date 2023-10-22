1f96250ab453e03304728c0551f6b2cd3442e272
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-22 17:58:43 +0500

    updated

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -8,12 +8,26 @@ const Navbar = () => {
 
         <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
             <Link  href="">
-                <Image src="/json-logo.svg"
+                <Image src="/jsm-logo.svg"
                  alt="logo"
                  width={55}
                  height={40}
                  />
             </Link>
+
+            <img src="/hamburger-menu.svg" 
+            width={30}
+            height={30}
+            alt="hamburger-menu"
+            className="block md:hidden" />
+
+            <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+                <li>
+                    <Link href="https://www.youtube.com/watch?v=3WCIyNOrzwM" target='_blank'>
+                        Next js Course</Link>
+                </li>
+
+            </ul>
 
         </div>
 
