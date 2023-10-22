bce60c33d333448edd63d46dfb9ff309107a5dd9
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-23 01:46:10 +0500

    updated

--- components/ui/Searchform.tsx
+++ components/ui/Searchform.tsx
@@ -1,17 +1,25 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from "@/components/ui/input"
 
 import { form } from 'sanity/desk'
 
 const Searchform = () => {
+    const [search, setSearch]=useState('');
   return (
-    <form>
-        <label>
+    <form className='flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5'>
+        <label className='flex-center relative w-full max-w-3xl'>
             <Image src="/magnifying-glass.svg" className="absolute left-8"height={32} width={32} 
             alt="Search-icon"/>
         
-        <Input />
+        <Input className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-500 " 
+        type="text"
+        placeholder="Serach"
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+        
+        />
 
         </label>
     </form>
