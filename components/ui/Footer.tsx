61d473e740cf7450d489eabf99eaf84cf2016030
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-22 23:42:58 +0500

    footer updated

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-120 py-12 max-md:flex-col ">
+      <p>Copyright © 2023 | All Rights Reserved</p>
+
+      <div className="flex gap-x-9">
+        <Link href="/terms-of-use">
+          Terms and Conditions
+        </Link>
+        <Link href="/privacy-policy">
+          Privacy Policy
+        </Link>
+
+
+      </div>
+    </footer>
   )
 }
 
