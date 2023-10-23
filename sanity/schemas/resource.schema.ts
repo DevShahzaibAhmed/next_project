4a032779379e9a9ea15406af269f60dd1f7ca4a7
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-23 22:20:11 +0500

    resource-schema updated

--- sanity/schemas/resource.schema.ts
+++ sanity/schemas/resource.schema.ts
@@ -1,5 +1,58 @@
-import { type SchemaTypeDefinition } from 'sanity'
+const schema={
+  name: 'resource',
+  title: 'Resource',
+  type: 'document',
+  field:[ 
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      require,
+      validation: (Rule:any) => Rule.required()
+    },
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+    {
+      name:'slug',
+      title: 'Slug',
+      type: 'slug',
+      Option:{source:'title'}
+    },
+
+    {
+      name:'downloadlink',
+      title: 'Download Link',
+      type: 'url',
+      Validations:(Rule:any) => Rule.required()
+    },
+
+    {
+      name:'views',
+      title: 'Views',
+      type:'number',
+      initalValue: 0,
+    },
+
+    {
+      name:'poster',
+      title:'Poster',
+      type:'image',
+      validation:(Rule:any) => Rule.required(),
+      Options: {
+      hotspot:true,
+      }
+    },
+
+    {
+      name:'category',
+      title:'Category',
+      type:'string',
+      validation:(Rule:any) => Rule.required(),
+      Options: {
+        list:['FrontEnd', 'Backend', 'FullStack', 'Others']
+      }
+
+    }
+
+  ]
+
 }
