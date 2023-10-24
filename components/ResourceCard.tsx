0da403e4309ad1a6f45a826ff9ab8771fd7c9458
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-25 02:23:46 +0500

    card installed

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -1,3 +1,12 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 interface Props {
     id:string;
     title:string;
