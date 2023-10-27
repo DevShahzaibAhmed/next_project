abbfaf5a0cbb4e9497f1fe931a8c361788343f4e
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-27 22:20:04 +0500

    updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/ui/Footer";
 import Navbar from "@/components/ui/Navbar";
-import React from "react";
+//import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
